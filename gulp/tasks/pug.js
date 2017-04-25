f33acef5bb9b2e7f2d2ef29732df6f0ca2dc4ea3
Sergey <starluk@mail.ru>
2017-04-26 00:06:57 +0300

    Verstka

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -7,7 +7,11 @@ module.exports = function() {
     patterns.push({ match: '%=version=%', replace: $.dev ? '' : `?rel=${$.package.version}` });//Math.ceil(Math.random()*100000)
 
     return $.gulp.src('./source/template/pages/*.pug')
-      .pipe($.gp.pug({ pretty: true }))
+      .pipe($.gp.wait(500))
+      .pipe($.gp.pug({ 
+        locals: JSON.parse($.fs.readFileSync('./content.json', 'utf8')),
+        pretty: true 
+      }))
       .on('error', $.gp.notify.onError(function(error) {
         return {
           title: 'Pug',
