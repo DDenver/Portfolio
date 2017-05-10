979aceba5039f674d02a9805dbc598d65fc5a909
Sergey <starluk@mail.ru>
2017-05-10 23:39:35 +0300

    Map&Flip

--- source/js/app.js
+++ source/js/app.js
@@ -1,17 +1,37 @@
-console.log('sss');
-$(function(){
-    console.log('dolll');
-        function initMap() {
-            console.log('dada');
-            var uluru = {lat: -25.363, lng: 131.044};
-            var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 4,
-            center: uluru
-            });
-            var marker = new google.maps.Marker({
-            position: uluru,
-            map: map
-            });
-        }
-} );
+google.maps.event.addDomListener(window, 'load', init);
+        
+            function init() {
+                var uluru = {lat: 53.9043111, lng: 27.5533251};
+                // Basic options for a simple Google Map
+                // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+                var mapOptions = {
+                    // How zoomed in you want the map to start at (always required)
+                    zoom: 15,
 
+                    // The latitude and longitude to center the map (always required)
+                    center: new google.maps.LatLng(53.9042858, 27.5416144), // New York
+                    disableDefaultUI: true,
+                    scrollwheel: false,
+                   styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#88a97c"},{"visibility":"on"}]}]
+                };
+
+                // Get the HTML DOM element that will contain your map 
+                // We are using a div with id="map" seen below in the <body>
+                var mapElement = document.getElementById('map');
+
+                // Create the Google Map using our element and options defined above
+                var map = new google.maps.Map(mapElement, mapOptions);
+
+                // Let's also add a marker while we're at it
+                var image = {
+                        url: '../assets/img/icons/map_marker.svg', 
+                        size: new google.maps.Size(44, 54)
+                    };
+                var marker = new google.maps.Marker({
+                    position: uluru,
+                    map: map,
+                    title: 'I',
+                    icon: image
+                });
+
+                }
